857276826f109747cfeb04f8854e4b127732ddf1
canyamax95 <khatrimaulik007@gmail.com>
2023-05-16 13:05:36 -0400

    This is my first git commit

---
+++ src/test/java/tests/GetAndPostExamples.java
@@ -0,0 +1,61 @@
+package tests;
+
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class GetAndPostExamples {
+	
+	//@Test
+	public void testGet() {
+		
+		baseURI = "https://reqres.in/api";
+		given().
+		get("/users?page=2").
+		then().
+		statusCode(200).body("data[4].first_name", equalTo("George")).
+		body("data.first_name",hasItems("George","Rachel"));
+		
+		
+	}
+	
+	@Test
+	public void testPost() {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		//map.put("name", "Maulik");
+		//map.put("job", "Mechanical");
+		
+		//System.out.println(map);
+		
+		JSONObject request = new JSONObject(map);
+		
+		request.put("name", "Maulik");
+		request.put("job", "Mechanical");
+		
+		System.out.println(request.toJSONString());
+		
+		baseURI = "https://reqres.in/api";
+		
+		given().
+		header("Content-Type", "application/json").
+		contentType(ContentType.JSON).
+		accept(ContentType.JSON).
+		body(request.toJSONString()).
+		when().
+		post("/users").
+		then().statusCode(201).log().all();
+	}
+	
+
+}
